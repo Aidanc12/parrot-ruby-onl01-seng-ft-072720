14da8c22ab4285d8fcf1ff5c5526f29c9c56e67e
Aidan cisneros <aidan_cisneros@yahoo.com>
2020-07-28 23:03:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # create method 'parrot' that outputs a givin phrase and
 # returns the prhase
-+
-+ def parrot(string="squawk!")
-+ puts "#{string}"
-+ "#{string}"
+
+ def parrot(string="squawk!")
+ puts "#{string}"
+ "#{string}"
 end 
